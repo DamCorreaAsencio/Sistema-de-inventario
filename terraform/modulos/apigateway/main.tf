dd42d7d768e359994f8d5bb7dc686ee0deff669b
DarkTrain06 <damerstefanocorreaasencio@gmail.com>
2025-12-04 21:18:34 -0500

    feat: Lo mismo de hace un momento, pero en general xd

--- terraform/modulos/apigateway/main.tf
+++ terraform/modulos/apigateway/main.tf
@@ -18,10 +18,14 @@ resource "aws_api_gateway_method" "proxy_method" {
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
+  
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 
 # Integración con ALB 
-resource "aws_api_gateway_integration" "proxy_integration" {
+resource "aws_api_gateway_integration" "proxy_integration" { //línea 24
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.proxy.id
   http_method             = aws_api_gateway_method.proxy_method.http_method
@@ -49,4 +53,36 @@ resource "aws_api_gateway_integration" "root_integration" {
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
   uri                     = "http://${var.alb_dns_name}/"
+}
+
+# Deployment
+resource "aws_api_gateway_deployment" "api_deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  depends_on = [
+    aws_api_gateway_integration.proxy_integration,
+    aws_api_gateway_integration.root_integration
+  ]
+
+  # Forzar nuevo deployment cada vez que cambie la configuración
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.proxy.id,
+      aws_api_gateway_method.proxy_method.id,
+      aws_api_gateway_integration.proxy_integration.id,
+      aws_api_gateway_method.root_method.id,
+      aws_api_gateway_integration.root_integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Stage
+resource "aws_api_gateway_stage" "api_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api_deploy.id
+  stage_name    = var.stage_name
 }
